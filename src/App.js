7aaaef435350d77d6ff9494aa604972b299046f4
Kene <kayyungx@gmail.com>
2022-11-21 21:20:33 +0100

    render country list and add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { fetchCountry } from '../store/action';
+import Nav from '../components/Nav';
+import euImg from '../assets/eu.png';
+import CountryList from '../components/CountryList';
 
 let retrieveOnce = true;
 const Home = () => {
-  const country = useSelector((state) => state.rocket, shallowEqual);
+  const country = useSelector((state) => state.country, shallowEqual);
 
   const dispatch = useDispatch();
 
@@ -17,7 +20,18 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Home page</h1>
+      <Nav>Continent</Nav>
+      <div className="flex p-5 bg-[#FC5193] items-center">
+        <img src={euImg} alt="Europe map" className="w-[60%] opacity-60" />
+        <div className="ml-6">
+          <h3 className="text-3xl font-mono text-white font-bold">Europe</h3>
+          <p className="text-xl text-white">746.4 million</p>
+        </div>
+      </div>
+      <div className="text-white bg-[#EC4C8A]">
+        <h3 className="ml-8">Stats by Country</h3>
+      </div>
+      <CountryList data={country} />
     </div>
   );
 };
